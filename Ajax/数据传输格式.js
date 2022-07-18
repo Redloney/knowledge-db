8ab28de38db1ae54af970bfc000dd9e058f58cb8
Redlonely <67591850@qq.com>
2022-07-18 19:36:58 +0800

    2022-7-18

---
+++ Ajax/数据传输格式.js
@@ -0,0 +1,16 @@
+// JSON
+
+var json = {
+  "version": "1.0",
+  "tags": [
+    {
+      "name": "JSON",
+      "description": "",
+      "example": ""
+    }
+  ]
+}
+
+var xml = `
+<xml version="1.0" encoding="UTF-8"?>
+`
